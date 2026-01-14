80c0f109c3e91f1a86dc9e7442c05513698827bc
odin kammerloher <odin@posthog.com>
2026-01-14 15:16:30 +0100

    feat(workflows): Add push subscription table & endpoints and use them for token CRUD

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5733,6 +5733,7 @@ export type CyclotronJobInputSchemaType = {
         | 'integration_field'
         | 'email'
         | 'native_email'
+        | 'push_subscription'
     key: string
     label: string
     choices?: { value: string; label: string }[]
@@ -5746,6 +5747,7 @@ export type CyclotronJobInputSchemaType = {
     integration_key?: string
     integration_field?: string
     requires_field?: string
+    platform?: 'android' | 'ios' | 'web'
     requiredScopes?: string
 }
 
